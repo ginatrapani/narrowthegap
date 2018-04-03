c634d42f5d1017bf286ebc61e94ef16f2b284b0b
Gina Trapani <ginatrapani@gmail.com>
2018-04-02 22:35:18 -0400

    feat: render occupations hierarchichally (#32)
    feat: add parent slugs to data
    
    Closes #30

--- src/components/AllOccupations.js
+++ src/components/AllOccupations.js
@@ -1,37 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import { Helmet } from "react-helmet";
 import GapAPI from "../GapAPI.js";
 import { Link } from "react-router-dom";
 import Header from "./Header.js";
 import Footer from "./Footer.js";
 
-const AllOccupations = () => (
-    <div className="App">
-        <Helmet title="All Occupations - Narrow the Gap" />
-        <div className="container">
-            <Header />
-            <div>
-                <h1>All Occupations</h1>
-                <ul>
-                    {GapAPI.all().map(
-                        g =>
-                            g.cents_to_dollar > 0 && g.cents_to_dollar < 100 ? (
-                                <li key={g.slug}>
-                                    <Link to={`/gap/${g.slug}`}>
-                                        {GapAPI.capitalize(
-                                            GapAPI.cleanOccupationName(
-                                                g.occupation_name
-                                            )
-                                        )}
-                                    </Link>
-                                </li>
-                            ) : null
+class AllOccupations extends Component {
+    renderChidren(g) {
+        return (
+            <li key={g.slug}>
+                <Link to={`/gap/${g.slug}`}>
+                    {GapAPI.capitalize(
+                        GapAPI.cleanOccupationName(g.occupation_name)
                     )}
+                </Link>
+                <ul>
+                    {GapAPI.gaps
+                        .filter(function(child) {
+                            return child.parent_slug === g.slug;
+                        })
+                        .map(g => this.renderChidren(g))}
                 </ul>
+            </li>
+        );
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Helmet title="All Occupations - Narrow the Gap" />
+                <div className="container">
+                    <Header />
+                    <div>
+                        <h1>All Occupations</h1>
+                        {GapAPI.gaps
+                            .filter(function(g) {
+                                return g.parent_slug === "";
+                            })
+                            .map(g => this.renderChidren(g))}
+                    </div>
+                    <Footer />
+                </div>
             </div>
-            <Footer />
-        </div>
-    </div>
-);
+        );
+    }
+}
 
 export default AllOccupations;
