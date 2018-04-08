4bbbedc905d491881005b9e6829f15b5a7c1c332
Gina Trapani <ginatrapani@gmail.com>
2018-04-08 17:17:52 -0400

    feat: relocated occupation list to front page

--- src/components/AllOccupations.js
+++ src/components/AllOccupations.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
-import { Helmet } from "react-helmet";
 import GapAPI from "../GapAPI.js";
-import { Link } from "react-router-dom";
-import Header from "./Header.js";
-import Footer from "./Footer.js";
 
 class AllOccupations extends Component {
     renderChidren(g) {
         return (
-            <ul>
+            <ul key={g.slug}>
                 <li key={g.slug}>
-                    <Link to={`/gap/${g.slug}`}>
+                    <a href={`/gap/${g.slug}`}>
                         {GapAPI.capitalize(
                             GapAPI.cleanOccupationName(g.occupation_name)
                         )}
-                    </Link>
+                    </a>
                     {GapAPI.gaps
                         .filter(function(child) {
                             return child.parent_slug === g.slug;
@@ -26,21 +22,14 @@ class AllOccupations extends Component {
     }
 
     render() {
+        var props = this.props;
         return (
-            <div className="App">
-                <Helmet title="All Occupations - Narrow the Gap" />
-                <div className="container">
-                    <Header />
-                    <div>
-                        <h1>All Occupations</h1>
-                        {GapAPI.gaps
-                            .filter(function(g) {
-                                return g.parent_slug === "";
-                            })
-                            .map(g => this.renderChidren(g))}
-                    </div>
-                    <Footer />
-                </div>
+            <div className="span4">
+                {GapAPI.gaps
+                    .filter(function(g) {
+                        return g.slug === props.columnSlug;
+                    })
+                    .map(g => this.renderChidren(g))}
             </div>
         );
     }
