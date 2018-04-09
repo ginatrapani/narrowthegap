1aa6b6ac9161306f8f84473998087c1b64c4e0d5
Matt Jacobs <capn@capndesign.com>
2018-04-08 22:59:02 -0400

    New fonts, new colors, new logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,39 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Header extends Component {
-    render() {
-        // @TODO Stop using inline CSS
-        var rowStyle = {
-            marginTop: "10px"
-        };
+  render() {
+    // @TODO Stop using inline CSS
+    var rowStyle = {
+      marginTop: '10px'
+    };
 
-        return (
-            <div className="row" style={rowStyle}>
-                <div className="col-md-3">
-                    <a
-                        href="/"
-                        role="button"
-                        className="btn btn-danger btn-lg active"
-                    >
-                        Narrow the Gap
-                    </a>
-                </div>
-                <div className="col-md-9" />
-                {/* /.col-lg-6 */}
-            </div>
-        );
-    }
+    var logoStyle = {
+      width: '160px',
+      height: 'auto',
+      margin: '0'
+    };
+
+    return (
+      <div className="row" style={rowStyle}>
+        <div className="col-md-3">
+          <a
+            href="/"
+            style={{
+              textDecoration: 'none',
+              border: '0'
+            }}
+          >
+            <img
+              src="/images/ntg-logo-white.png"
+              alt="Narrow the Gap"
+              style={logoStyle}
+            />
+          </a>
+        </div>
+        <div className="col-md-9" />
+        {/* /.col-lg-6 */}
+      </div>
+    );
+  }
 }
 export default Header;
