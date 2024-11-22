aaa3f427f5170dcf5ca311babbb6f065e546102f
Gina Trapani <ginatrapani@gmail.com>
2024-11-22 11:39:40 -0500

    chore: upgrade deps
    - from react-snapshot to react-snap
    - react
    - Node 22 for Netlify
    - yarn version
    - fix envelope error https://stackoverflow.com/questions/69692842/error-message-error0308010cdigital-envelope-routinesunsupported

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "react-snapshot";
+import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
